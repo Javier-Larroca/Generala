01c656c12cad5d53ea2f70cb2de8ef5cb934e553
Javier Agustin Larroca <javier-larroca@hotmail.com>
2020-06-21 20:59:17 -0300

    Creacion de las 3 primeras funciones y agregadas en main

--- tpi_generala/funciones.h
+++ tpi_generala/funciones.h
@@ -1,1 +1,38 @@
+#include<iostream>
+#include<cstdlib>
+#include<cstdio>
+#include<ctime>
+using namespace std;
 
+/// DECLARACIÓN/PROTOTIPO DE FUNCIONES
+
+void tirarDados(int v[]); //Carga en un vector de 5 posiciones(dados) valores al azar del 1 al 6
+
+void mostrarDados(int v[]); //Muestra el vector(dados) de 5 posiciones
+
+void cambiarUnDado(int v[], int pos); //Cambia el valor de un dado en especifico (Se ingresa directamente el nro de dado cargado por el usuario)
+
+/// DESARROLLO/ DEFINICIONES DE FUNCIONES
+
+void tirarDados(int v[]){
+    int i;
+    srand(time(NULL));
+    for( i=0; i<5; i++ ){
+        v[i]=(rand()%6)+1;
+    }
+}
+
+void mostrarDados(int v[]){
+    int i;
+    cout<<endl<<"   1            2            3            4            5   ";
+    cout<<endl<<"-------      -------      -------      -------      -------";
+    cout<<endl<<"|     |      |     |      |     |      |     |      |     |";
+    cout<<endl<<"|  "<<v[0]<<"  |      |  "<<v[1]<<"  |      |  "<<v[2]<<"  |      |  "<<v[3]<<"  |      |  "<<v[4]<<"  |";
+    cout<<endl<<"|     |      |     |      |     |      |     |      |     |";
+    cout<<endl<<"-------      -------      -------      -------      -------";
+}
+
+void cambiarUnDado(int v[], int pos){
+    srand(time(NULL));
+    v[pos-1]=(rand()%6)+1;
+}
--- tpi_generala/main.cpp
+++ tpi_generala/main.cpp
@@ -1,9 +1,33 @@
-    #include <iostream>
+#include <iostream>
+#include <cstdlib>
+#include <cstdio>
+#include <ctime>
 
 using namespace std;
 
+#include "funciones.h"
+
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    int vec_dado1[5], cant_dados, nro_dado;
+    char sn;
+
+    tirarDados(vec_dado1);
+    cout<<endl<<"Dados de jugador 1: "<<endl;
+    mostrarDados(vec_dado1);
+    cout<<endl<<"¿Desea volver a tirar algun dado? S/N ";
+    cin>>sn;
+    if (sn=='s'|| sn=='S'){
+        cout<<"¿Cuantos dados quiere volver a tirar? ";
+        cin>>cant_dados;
+        for (int x=0; x<cant_dados; x++){
+            cout<<"¿Que nro de dado desea tirar nuevamente? ";
+            cin>>nro_dado;
+            cambiarUnDado(vec_dado1, nro_dado);
+        }
+        mostrarDados(vec_dado1);
+    }
+
+
 }
+
